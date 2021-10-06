798bc3a10bf692651cb7324794766757614ae907
Norbert Lesniak <s20151@pjwstk.edu.pl>
2021-10-06 14:29:15 +0200

    Lab01 - v0.1

--- lab01/main.cpp
+++ lab01/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <tuple>
+#include <functional>
+
+auto obliczenia = [](auto f1, auto f2, int n) {
+    int max_x = f1(0);
+    int max_y = f2(max_x);
+    for(int i=1; i<n; i++){
+        int x = f2(i);
+        int y = f2(x);
+        if (y > max_y) {
+            max_x = y;
+            max_y = y;
+        }
+    }
+    int pkt[2] = {max_x, max_y};
+    return pkt;
+};
+
+std::function<int(int)> f1 = [](int x){ return x; };
+std::function<int(int)> f2 = [](int x){ return x; };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
