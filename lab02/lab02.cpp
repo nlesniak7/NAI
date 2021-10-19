afdc76be0b3dcdb4bcbce118d448a15fcd9ad597
s20151 <s20151@pjwstk.edu.pl>
2021-10-19 23:14:47 +0200

    home

--- lab02/lab02.cpp
+++ lab02/lab02.cpp
@@ -54,29 +54,47 @@ auto three_hump_camel_function_domain = [](vector<double> v) {
     return (abs(v[0]) <= 5) && (abs(v[1]) <= 5);
 };
 
-auto bukin_function_n6 = [](vector<double> v) {
+auto eggholder_function = [](vector<double> v) {
     double x = v.at(0), y = v.at(1);
-    return 100* sqrt(abs(y-(0.01* pow(x,2))))+0.01* abs(x+10);
+    return -1*(y+47) * sin(sqrt(abs(x/2+(y+47))))
+    - x * sin(sqrt(abs(x-(y+47))));
 };
 
-auto bukin_function_n6_domain = [](vector<double> v) {
-    return (v[0] >= -15) && (v[0]<= -5) && (abs(v[1]) <= 3);
+auto eggholder_function_domain = [](vector<double> v) {
+    return (abs(v[0]) <= 512) && (abs(v[1]) <= 512);
 };
 
 int main(){
-//    int iterations = 0;
-//    cout<<"Iterations amount: ";
-//    cin>> iterations;
-//    while(iterations<=0){
-//        cout<<"iterations amount must be > 0";
-//        cin>>iterations;
-//    }
-    uniform_real_distribution<> distrib_r(-5, 5);
-    vector<double> three_hump_p0 = {
-            0,
-            0,
-    };
-    auto r = hill_climbing(three_hump_camel_function, three_hump_camel_function_domain, three_hump_p0, 100);
-    cout<< r << " >>> " << three_hump_camel_function(r)<<endl;
+    int user_input = 0;
+    cout<<"Pick function: 1 - Three hump camel function, 2 - Eggholder function"<<endl;
+    cin>>user_input;
+    while(user_input!=1 && user_input!=2){
+        cout<<"Pick 1 or 2"<<endl;
+        cin>>user_input;
+    }
+    int iterations = 0;
+    cout<<"Iterations amount: "<<endl;
+    cin>> iterations;
+    while(iterations<1){
+        cout<<"Iterations amount must be bigger than 0";
+        cin>>iterations;
+    }
+    if(user_input==1){
+        uniform_real_distribution<> distrib_r(-5, 5);
+        vector<double> three_hump_p0 = {
+                distrib_r(gen),
+                distrib_r(gen),
+        };
+        auto r = hill_climbing(three_hump_camel_function, three_hump_camel_function_domain, three_hump_p0, iterations);
+        cout<< r << " >>> " << three_hump_camel_function(r)<<endl;
+    }else{
+        uniform_real_distribution<> distrib_r(-512, 512);
+        vector<double> eggholder_function_p0 = {
+                distrib_r(gen),
+                distrib_r(gen),
+        };
+        auto r = hill_climbing(eggholder_function, eggholder_function_domain, eggholder_function_p0, iterations);
+        cout<< r << " >>> " << eggholder_function(r)<<endl;
+    }
 
 }
