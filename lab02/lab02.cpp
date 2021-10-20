7523f5c5a35047c71feeed920d30502f2fda72fa
Norbert Lesniak <norbert.lesniak7@gmail.com>
2021-10-20 11:50:42 +0200

    update2

--- lab02/lab02.cpp
+++ lab02/lab02.cpp
@@ -29,10 +29,10 @@ vector<double> operator+(vector<double> a, vector<double> b)
 random_device rd;  //Will be used to obtain a seed for the random number engine
 mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
 
-vector<double> hill_climbing(function<double(vector<double>)> f, function<bool(vector<double>)> f_domain, vector<double> p0, int iterations, uniform_real_distribution<double> shift){
+vector<double> hill_climbing(function<double(vector<double>)> f, vector<double> p0, int iterations, uniform_real_distribution<double> shift){
     auto p = p0;
     uniform_int_distribution<> distrib(0, p.size()-1);
-    if (!f_domain(p)) throw std::invalid_argument("The p0 point must be in domain");
+
     for (int i = 0; i < iterations; i++) {
         auto p2 = p;
 
@@ -50,18 +50,10 @@ auto three_hump_camel_function = [](vector<double> v) {
     return 2*(pow(x,2)) - 1.05*(pow(x,4)) + (pow(x,6)/6) + x*y + pow(y,2);
 };
 
-auto three_hump_camel_function_domain = [](vector<double> v) {
-    return (abs(v[0]) <= 5) && (abs(v[1]) <= 5);
-};
-
 auto eggholder_function = [](vector<double> v) {
     double x = v.at(0), y = v.at(1);
     return -1*(y+47) * sin(sqrt(abs(x/2+(y+47))))
            - x * sin(sqrt(abs(x-(y+47))));
-};
-
-auto eggholder_function_domain = [](vector<double> v) {
-    return (abs(v[0]) <= 512) && (abs(v[1]) <= 512);
 };
 
 int main(){
@@ -90,7 +82,7 @@ int main(){
                 hump_r(gen),
                 hump_r(gen),
         };
-        auto r = hill_climbing(three_hump_camel_function, three_hump_camel_function_domain, three_hump_p0, iterations, shift);
+        auto r = hill_climbing(three_hump_camel_function, three_hump_p0, iterations, shift);
         cout <<"f( "<< r << " ) = " << three_hump_camel_function(r) << endl;
     }else{
         uniform_real_distribution<> eggholder_r(-512, 512);
@@ -98,8 +90,8 @@ int main(){
                 eggholder_r(gen),
                 eggholder_r(gen),
         };
-        auto r = hill_climbing(eggholder_function, eggholder_function_domain, eggholder_function_p0, iterations, shift);
-        cout <<"f( "<< r << ") = " << eggholder_function(r) << endl;
+        auto r = hill_climbing(eggholder_function, eggholder_function_p0, iterations, shift);
+        cout <<"min. f( "<< r << ") = " << eggholder_function(r) << endl;
     }
 
 }
